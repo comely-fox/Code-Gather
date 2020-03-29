e1f295b2c27eab329e93c0dfd2f2bafa18426945
comely-fox <707893615@qq.com>
2020-03-29 14:26:25 +0800

    增加webpack demo; module: "scss, css, postcss"

---
+++ webpack/2020/3/28/README.md
@@ -0,0 +1,14 @@
+####tip:
+
+folder:
+
+- scss, image, font, data, ... 管理资源， 解析非 js 模块
+  1. scss: [style-loader, css-loader, sass-loader, postcss-loader]
+  2. image: [url-loader, file-loader, image-webpack-loader, responsive-loader]
+  3. font: [url-loader, file-loader, font-awesome]
+  4. data: [csv-loader, xml-loader]
+- output - 管理输出
+  1. html-webpack-plugin 生成 html 文件
+  2. mini-css-extract-plugin 提取 css 到文件
+  3. clean-webpack-plugin 删除 dist 目录
+  4. webpack-manifest-plugin 生成 manifest 文件
---
+++ webpack/2020/3/28/scss/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('autoprefixer')({
+                  overrideBrowserslist: ' > 0.002%'
+                })
+              ],
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
